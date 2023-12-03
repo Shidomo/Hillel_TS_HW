47e2905f57169332f7faec4c3f5ce4744cf5b75a
Shidomo <shindor.hardcore@gmail.com>
2023-12-03 17:48:31 +0200

    hw_3

---
+++ hw3/src/index.ts
@@ -0,0 +1,70 @@
+const BadgeSize = {
+	single: '4x3',
+	double: '4x6'
+}
+
+const Print = {
+	standart: 'color',
+	fast: 'zpl'
+}
+
+enum BadgeTypesEnum {
+	COLOR = 'color',
+	MONO = 'mono'
+}
+
+type BadgeSizeType = keyof typeof BadgeSize;
+type PrintType = keyof typeof Print;
+
+
+class Student {
+	badgeTypeMap:Map<string,BadgeTypesEnum> = new Map<string,  BadgeTypesEnum>([
+		['single_fast', BadgeTypesEnum.COLOR],
+		['single_standart', BadgeTypesEnum.COLOR],
+		['double_fast', BadgeTypesEnum.MONO],
+		['double_standart', BadgeTypesEnum.MONO]
+	])
+
+	private _firstName : string;
+	private _lastName : string;
+	private _birthYear : number;
+	private _grades:{workName:string;mark:boolean}[] = []; // Опишите, как объект у которого есть поле workName и mark(оценка может быть выполненно или нет)
+	private _visits:{ lesson: string; present: boolean }[] = []; // Опишите, как объект у которого есть поле lesson (любое имя) и present
+
+	get fullName():string {
+		return `${this._lastName} ${this._firstName}`;
+	}
+	
+	set fullName(value:string) {
+		[this._lastName, this._firstName] = value.split(' ');
+	}
+
+	get age():number {
+		return new Date().getFullYear() - this._birthYear;
+	}
+
+	constructor(private firstName:string, private lastName:string, private birthYear:number) {
+	}
+
+	setGrade(grade: {workName:string,mark:boolean}):void {
+		this._grades.push(grade);
+	}
+
+	setVisit(visit: {lesson:string,present:boolean}):void {
+		this._visits.push(visit);
+	}
+
+	getPerformanceRating(): number {
+		if (this._grades.length === 0) return 0;
+
+		const totalMarks = this._grades.reduce((sum, grade) => sum + (grade.mark ? 1 : 0), 0);
+		const averageGrade = totalMarks / this._grades.length;
+
+		const totalVisits = this._visits.length;
+		const presentVisits = this._visits.filter(visit => visit.present).length;
+		const attendancePercentage = (presentVisits / totalVisits) * 100;
+
+		return (averageGrade + attendancePercentage) / 2;
+	}
+
+}
