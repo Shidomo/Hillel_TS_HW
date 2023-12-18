7f5587d8f6fac8dea42d2ec376980f84c83e8cdc
Shidomo <shindor.hardcore@gmail.com>
2023-12-18 16:31:20 +0200

    hw_7

--- hw7/src/objTypeIndex.ts
+++ hw7/src/objTypeIndex.ts
@@ -6,10 +6,26 @@ interface MyUnion {
 interface IFunc {
   [key: string]: (...args: any[]) => any;
 }
+
+function anyFuncNumber(numb: number): void {
+  console.log(numb);
+}
+
+const functionMap: IFunc = {
+  myFunction: anyFuncNumber,
+};
+
+functionMap.myFunction(13);
 //Опишіть інтерфейс, який використовує сигнатуру індексу для опису об'єкта, подібного до масиву. Ключі повинні бути числами, а значення - певного типу.
 interface INumb {
   [key: number]: number;
 }
+
+const anyObj: INumb = {
+  0: 10,
+  20: 30,
+};
+console.log(anyObj);
 //Створіть інтерфейс з певними властивостями та індексною сигнатурою. Наприклад, ви можете мати властивості типу name: string та індексну сигнатуру для додаткових динамічних властивостей.
 interface MyObject {
   name: string;
@@ -23,6 +39,26 @@ interface BaseInterface {
 interface ExtendedInterface extends BaseInterface {
   anyProperty: string;
 }
+
+class ForInterface implements ExtendedInterface {
+  // Властивості з BaseInterface
+  [key: string]: string;
+
+  // Властивість з ExtendedInterface
+  anyProperty: string;
+
+  // Додаткові властивості для класу ForInterface
+  name: string;
+
+  constructor(name: string, anyProperty: string) {
+    this.name = name;
+    this.anyProperty = anyProperty;
+  }
+}
+const instance = new ForInterface("John", "Some value");
+console.log(instance.name);
+console.log(instance.anyProperty);
+
 //Напишіть функцію, яка отримує об'єкт з індексною сигнатурою і перевіряє, чи відповідають значення певних ключів певним критеріям (наприклад, чи всі значення є числами).
 const areAllValuesNumbers = (obj: MyObject): boolean => {
   for (const key in obj) {
